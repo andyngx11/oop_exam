7972cfab0d3f97fa5196337ce65bff7d53647349
DucNam11 <namyhsd1@gmail.com>
2025-06-01 18:59:32 +0700

    update

---
+++ midtern-OOP/datastructure/MyLinkedList.java
@@ -0,0 +1,95 @@
+package hus.oop.datastructure;
+
+public class MyLinkedList extends AbstractMyList {
+    private Node head;
+    private int size;
+
+    /**
+     * Hàm dựng khởi tạo list để chứa dữ liệu.
+     */
+    public MyLinkedList() {
+        /* TODO */
+    }
+
+    @Override
+    public int size() {
+        /* TODO */
+    }
+
+    /**
+     * Lấy giá trị của phần tử ở vị trí index.
+     * @param index
+     * @return
+     */
+    @Override
+    public int get(int index) {
+        /* TODO */
+    }
+
+    /**
+     * Sửa dữ liệu ở vị trí index thành data.
+     * @param data
+     * @param index
+     */
+    @Override
+    public void set(int data, int index) {
+        /* TODO */
+    }
+
+    /**
+     * Thêm phần tử dữ liệu vào đầu tập dữ liệu.
+     * @param value giá trị của phần tử dữ liệu được thêm vào.
+     */
+    @Override
+    public void insertAtStart(int value) {
+        /* TODO */
+    }
+
+    /**
+     * Thêm phần tử dữ liệu vào cuối tập dữ liệu.
+     * @param value giá trị của phần tử dữ liệu được thêm vào.
+     */
+    @Override
+    public void insertAtEnd(int value) {
+        /* TODO */
+    }
+
+    /**
+     * Thêm phần tử dữ liệu vào vị trí index của tập dữ liệu.
+     * Chỉ thêm được nếu index nằm trong đoạn [0 - size()].
+     * @param value
+     * @param index
+     */
+    @Override
+    public void insertAtPosition(int value, int index) {
+        /* TODO */
+    }
+
+    /**
+     * Xóa phần tử dữ liệu tại vị trí index.
+     * Chỉ xóa được nếu index nằm trong đoạn [0 - (size() - 1)]
+     * @param index
+     */
+    @Override
+    public void remove(int index) {
+        /* TODO */
+    }
+
+    /**
+     * Trả về Node ở vị trí index.
+     * @param index
+     * @return
+     */
+    private Node getNodeByIndex(int index) {
+        /* TODO */
+    }
+
+    /**
+     * Lấy ra dữ liệu được lưu theo cấu trúc dữ liệu kiểu mảng.
+     * @return
+     */
+    @Override
+    public int[] toArray() {
+        /* TODO */
+    }
+}
---
+++ midtern-OOP/statistics/ListDataSet.java
@@ -0,0 +1,66 @@
+package hus.oop.statistics;
+
+import java.util.List;
+
+public class ListDataSet extends AbstractDataSet {
+    private List<Double> data;
+
+    /**
+     * Hàm dựng khởi tạo list để chứa dữ liệu.
+     */
+    public ListDataSet() {
+        /* TODO */
+    }
+
+    @Override
+    public int size() {
+        /* TODO */
+    }
+
+    @Override
+    public double getAt(int index) {
+        /* TODO */
+    }
+
+    @Override
+    public double[] getAll(int from, int to) {
+        /* TODO */
+    }
+
+    /**
+     * Thêm phần tử dữ liệu vào cuối tập dữ liệu.
+     * @param value giá trị của phần tử dữ liệu được thêm vào.
+     */
+    @Override
+    public void append(double value) {
+        /* TODO */
+    }
+
+    /**
+     * Thêm phần tử dữ liệu vào vị trí index của tập dữ liệu.
+     * @param index
+     * @param value
+     */
+    @Override
+    public void insert(int index, double value) {
+        /* TODO */
+    }
+
+    /**
+     * Xóa phần tử dữ liệu tại vị trí index.
+     * @param index
+     */
+    @Override
+    public void remove(int index) {
+        /* TODO */
+    }
+
+    /**
+     * Xóa tất cả các phần tử dữ liệu có giá trị bằng value trong tập dữ liệu.
+     * @param value
+     */
+    @Override
+    public void remove(double value) {
+        /* TODO */
+    }
+}
